d1871b69879234d45ba237a5b2a086f9dd1496db
muhmad <muhmad@live.no>
2024-07-15 23:59:31 +0200

    test

--- news8a841d781095471c/__init__.py
+++ news8a841d781095471c/__init__.py
@@ -1,20 +1,27 @@
-import json
-import random
 import aiohttp
-from aiohttp_socks import ProxyConnector
-from aiohttp import ClientSession, ClientTimeout
+import hashlib
+import logging
 from datetime import datetime as datett, timedelta, timezone
 from dateutil import parser
-import logging
-from typing import AsyncGenerator, List
+from typing import AsyncGenerator
 import tldextract as tld
-import hashlib
-import asyncio
+import random
+import json
+from aiohttp_socks import ProxyConnector
+from aiohttp import ClientSession, ClientTimeout
 
-from exorde_data import Item, Content, Author, CreatedAt, Title, Url, Domain, ExternalId
+from exorde_data import (
+    Item,
+    Content,
+    Author,
+    CreatedAt,
+    Title,
+    Url,
+    Domain,
+    ExternalId,
+)
 
-# Default parameters
-DEFAULT_OLDNESS_SECONDS = 3600 * 3  # 3 hours
+DEFAULT_OLDNESS_SECONDS = 3600*3  # 3 hours
 DEFAULT_MAXIMUM_ITEMS = 10
 DEFAULT_MIN_POST_LENGTH = 10
 BASE_TIMEOUT = 30
@@ -23,16 +30,19 @@ USER_AGENT_LIST = [
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
 ]
 
-logging.basicConfig(level=logging.INFO)
-
-async def fetch_data(session, url, headers=None):
+async def fetch_data(url, proxy, headers=None):
+    connector = ProxyConnector.from_url(f"socks5://{proxy[0]}:{proxy[1]}", rdns=True)
+    timeout = ClientTimeout(total=BASE_TIMEOUT)
     try:
-        async with session.get(url, headers=headers) as response:
-            if response.status == 200:
-                return await response.json(content_type=None)
-            else:
-                logging.error(f"Error fetching data: {response.status} {await response.text()}")
-                return []
+        async with ClientSession(connector=connector, headers={"User-Agent": random.choice(USER_AGENT_LIST)}, timeout=timeout) as session:
+            async with session.get(url, headers=headers) as response:
+                response_text = await response.text()
+                if response.status == 200:
+                    json_data = await response.json(content_type=None)  # Manually handle content type
+                    return json_data
+                else:
+                    logging.error(f"Error fetching data: {response.status} {response_text}")
+                    return []
     except Exception as e:
         logging.error(f"Error fetching data: {e}")
         return []
@@ -59,77 +69,78 @@ def read_parameters(parameters):
         min_post_length = DEFAULT_MIN_POST_LENGTH
     return max_oldness_seconds, maximum_items_to_collect, min_post_length
 
-async def create_session_with_proxy(ip, port):
-    connector = ProxyConnector.from_url(f"socks5://{ip}:{port}", rdns=True)
-    timeout = ClientTimeout(total=BASE_TIMEOUT)
-    session = ClientSession(connector=connector, headers={"User-Agent": random.choice(USER_AGENT_LIST)}, timeout=timeout)
-    logging.info(f"Created session with proxy {ip}:{port}")
-    return session, f"{ip}:{port}"
-
-async def fetch_and_process(session, feed_url, max_oldness_seconds, min_post_length, maximum_items_to_collect):
-    data = await fetch_data(session, feed_url)
-    if not data:
-        return []
-
-    sorted_data = sorted(data, key=lambda x: x["pubDate"], reverse=True)
-    sorted_data = [entry for entry in sorted_data if is_within_timeframe_seconds(convert_to_standard_timezone(entry["pubDate"]), max_oldness_seconds)]
-    sorted_data = random.sample(sorted_data, int(len(sorted_data) * 0.3))
-
-    items = []
-    for entry in sorted_data:
-        if random.random() < 0.25:
-            continue
-
-        pub_date = convert_to_standard_timezone(entry["pubDate"])
-
-        if is_within_timeframe_seconds(pub_date, max_oldness_seconds):
-            sha1 = hashlib.sha1()
-            author = entry["creator"][0] if entry.get("creator") else "anonymous"
-            sha1.update(author.encode())
-            author_sha1_hex = sha1.hexdigest()
-
-            content_article_str = entry.get("content") or entry.get("description") or entry["title"]
-            domain_str = tld.extract(entry["source_url"] or "unknown").registered_domain
-            
-            new_item = Item(
-                content=Content(str(content_article_str)),
-                author=Author(str(author_sha1_hex)),
-                created_at=CreatedAt(pub_date),
-                title=Title(entry["title"]),
-                domain=Domain(str(domain_str)),
-                url=Url(entry["link"]),
-                external_id=ExternalId(entry["article_id"])
-            )
-
-            if len(new_item.content) >= min_post_length and len(items) < maximum_items_to_collect:
-                items.append(new_item)
-        else:
-            break
-    return items
-
 async def query(parameters: dict) -> AsyncGenerator[Item, None]:
     feed_url = 'https://raw.githubusercontent.com/user1exd/rss_realtime_feed/main/data/feed.json'
+    proxies = load_proxies('/exorde/ips.txt')
     max_oldness_seconds, maximum_items_to_collect, min_post_length = read_parameters(parameters)
     logging.info(f"[News stream collector] Fetching data from {feed_url} with parameters: {parameters}")
-    
-    proxies = load_proxies('/exorde/ips.txt')
-    sessions = await asyncio.gather(*[create_session_with_proxy(ip, port) for ip, port in proxies])
+    yielded_items = 0
 
-    try:
-        tasks = [fetch_and_process(session, feed_url, max_oldness_seconds, min_post_length, maximum_items_to_collect) for session, _ in sessions]
-        results = await asyncio.gather(*tasks)
+    while yielded_items < maximum_items_to_collect:
+        proxy = random.choice(proxies)
+        logging.info(f"Using proxy {proxy}")
+        data = await fetch_data(feed_url, proxy)
 
-        yielded_items = 0
-        for items in results:
-            for item in items:
-                if yielded_items >= maximum_items_to_collect:
-                    return
-                yield item
+        if not data:
+            continue
+
+        sorted_data = sorted(data, key=lambda x: x["pubDate"], reverse=True)
+        sorted_data = [entry for entry in sorted_data if is_within_timeframe_seconds(convert_to_standard_timezone(entry["pubDate"]), max_oldness_seconds)]
+        logging.info(f"[News stream collector] Filtered data : {len(sorted_data)}")
+
+        sorted_data = random.sample(sorted_data, int(len(sorted_data) * 0.3))
+
+        successive_old_entries = 0
+
+        for entry in sorted_data:
+            if random.random() < 0.25:
+                continue
+            logging.info(f"[News stream collector] Processing entry: {entry['title']} - {entry['pubDate']} - {entry['source_url']}")
+            if yielded_items >= maximum_items_to_collect:
+                break
+
+            pub_date = convert_to_standard_timezone(entry["pubDate"])
+
+            if is_within_timeframe_seconds(pub_date, max_oldness_seconds):
+                sha1 = hashlib.sha1()
+                author = entry["creator"][0] if entry.get("creator") else "anonymous"
+                sha1.update(author.encode())
+                author_sha1_hex = sha1.hexdigest()
+
+                content_article_str = ""
+                if entry.get("content"):
+                    content_article_str = entry["content"]
+                elif entry.get("description"):
+                    content_article_str = entry["description"]
+                else:
+                    content_article_str = entry["title"]
+
+                domain_str = entry["source_url"] if entry.get("source_url") else "unknown"
+                domain_str = tld.extract(domain_str).registered_domain
+                
+                new_item = Item(
+                    content=Content(str(content_article_str)),
+                    author=Author(str(author_sha1_hex)),
+                    created_at=CreatedAt(pub_date),
+                    title=Title(entry["title"]),
+                    domain=Domain(str(domain_str)),
+                    url=Url(entry["link"]),
+                    external_id=ExternalId(entry["article_id"])
+                )
+
                 yielded_items += 1
-    finally:
-        for session, _ in sessions:
-            await session.close()
-            await asyncio.sleep(0.1)  # Add delay between each request
+                yield new_item
+            else:
+                dt = datett.strptime(pub_date, "%Y-%m-%dT%H:%M:%S.00Z")
+                dt = dt.replace(tzinfo=timezone.utc)
+                current_dt = datett.now(timezone.utc)
+                time_diff = current_dt - dt
+                logging.info(f"[News stream collector] Entry is {abs(time_diff)} old : skipping.")
+                
+                successive_old_entries += 1
+                if successive_old_entries >= 5:
+                    logging.info(f"[News stream collector] Too many old entries. Stopping.")
+                    break
     logging.info(f"[News stream collector] Done.")
 
 def load_proxies(file_path):
