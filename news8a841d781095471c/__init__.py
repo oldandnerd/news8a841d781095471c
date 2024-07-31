ac16be6ad63788be4b532eeee6b0b30725235e20
muhmad <muhmad@live.no>
2024-07-31 13:57:16 +0200

    te

--- news8a841d781095471c/__init__.py
+++ news8a841d781095471c/__init__.py
@@ -1,11 +1,12 @@
 import aiohttp
 import hashlib
 import logging
+import asyncio
 from datetime import datetime as datett, timedelta, timezone
 from dateutil import parser
 from typing import AsyncGenerator
 import tldextract as tld
-import json
+import random
 
 from exorde_data import (
     Item,
@@ -18,7 +19,7 @@ from exorde_data import (
     ExternalId,
 )
 
-DEFAULT_OLDNESS_SECONDS = 3600 * 3  # 3 hours
+DEFAULT_OLDNESS_SECONDS = 3600*3  # 3 hours
 DEFAULT_MAXIMUM_ITEMS = 10
 DEFAULT_MIN_POST_LENGTH = 10
 
@@ -41,13 +42,6 @@ def convert_to_standard_timezone(_date):
     dt = parser.parse(_date)
     return dt.strftime("%Y-%m-%dT%H:%M:%S.00Z")
 
-def is_within_timeframe_seconds(dt_str, timeframe_sec):
-    dt = datett.strptime(dt_str, "%Y-%m-%dT%H:%M:%S.00Z")
-    dt = dt.replace(tzinfo=timezone.utc)
-    current_dt = datett.now(timezone.utc)
-    time_diff = current_dt - dt
-    return abs(time_diff) <= timedelta(seconds=timeframe_sec)
-
 def read_parameters(parameters):
     if parameters and isinstance(parameters, dict):
         max_oldness_seconds = parameters.get("max_oldness_seconds", DEFAULT_OLDNESS_SECONDS)
@@ -60,46 +54,64 @@ def read_parameters(parameters):
     return max_oldness_seconds, maximum_items_to_collect, min_post_length
 
 async def query(parameters: dict) -> AsyncGenerator[Item, None]:
-    feed_url = 'https://raw.githubusercontent.com/user1exd/rss_realtime_feed/main/data/feed.json'
+    max_oldness_seconds, maximum_items_to_collect, min_post_length = read_parameters(parameters)
+    feed_url = f'http://192.227.159.4:8000/get_articles?size={maximum_items_to_collect}'  # Adjust this URL to match your server's endpoint
     data = await fetch_data(feed_url)
 
-    max_oldness_seconds, maximum_items_to_collect, min_post_length = read_parameters(parameters)
     logging.info(f"[News stream collector] Fetching data from {feed_url} with parameters: {parameters}")
+    yielded_items = 0
 
-    sorted_data = sorted(data, key=lambda x: x["pubDate"], reverse=True)
-    valid_data = [entry for entry in sorted_data if is_within_timeframe_seconds(convert_to_standard_timezone(entry["pubDate"]), max_oldness_seconds)]
+    logging.info(f"[News stream collector] Total data received: {len(data)}")
 
-    # Divide the valid data into chunks of 15 articles each
-    chunk_size = 15
-    for i in range(0, len(valid_data), chunk_size):
-        chunk = valid_data[i:i + chunk_size]
-        for entry in chunk:
-            if len(entry.get("content", "")) < min_post_length:
-                continue
+    if not data:
+        logging.warning("Fetched data is empty. Retrying after 60 seconds.")
+        await asyncio.sleep(60)
+        return
 
-            pub_date = convert_to_standard_timezone(entry["pubDate"])
+    sorted_data = random.sample(data, int(len(data) * 0.3))
 
-            sha1 = hashlib.sha1()
-            author = entry["creator"][0] if entry.get("creator") else "anonymous"
-            sha1.update(author.encode())
-            author_sha1_hex = sha1.hexdigest()
+    successive_old_entries = 0
 
-            content_article_str = entry.get("content") or entry.get("description") or entry.get("title")
-
-            domain_str = entry.get("source_url") or "unknown"
-            domain_str = tld.extract(domain_str).registered_domain
-
-            new_item = Item(
-                content=Content(str(content_article_str)),
-                author=Author(str(author_sha1_hex)),
-                created_at=CreatedAt(pub_date),
-                title=Title(entry["title"]),
-                domain=Domain(str(domain_str)),
-                url=Url(entry["link"]),
-                external_id=ExternalId(entry["article_id"])
-            )
-
-            yield new_item
-        if len(valid_data) <= chunk_size:
+    for entry in sorted_data:
+        if random.random() < 0.25:
+            continue
+        logging.info(f"[News stream collector] Processing entry: {entry['title']} - {entry['pubDate']} - {entry['source_url']}")
+        if yielded_items >= maximum_items_to_collect:
             break
+
+        pub_date = convert_to_standard_timezone(entry["pubDate"])
+
+        sha1 = hashlib.sha1()
+        author = entry["creator"][0] if entry.get("creator") else "anonymous"
+        sha1.update(author.encode())
+        author_sha1_hex = sha1.hexdigest()
+
+        content_article_str = ""
+        # if no content (null), then if description is not null, use it as content
+        # else if description is null as well, then use title as content
+        if entry.get("content"):
+            content_article_str = entry["content"]
+        elif entry.get("description"):
+            content_article_str = entry["description"]
+        else:
+            content_article_str = entry["title"]
+
+        domain_str = entry["source_url"] if entry.get("source_url") else "unknown"
+        # remove the domain using tldextract to have only the domain name
+        # e.g. http://www.example.com -> example.com
+        domain_str = tld.extract(domain_str).registered_domain
+
+        new_item = Item(
+            content=Content(str(content_article_str)),
+            author=Author(str(author_sha1_hex)),
+            created_at=CreatedAt(pub_date),
+            title=Title(entry["title"]),
+            domain=Domain(str(domain_str)),
+            url=Url(entry["link"]),
+            external_id=ExternalId(entry["article_id"])
+        )
+
+        yielded_items += 1
+        yield new_item
+
     logging.info(f"[News stream collector] Done.")
